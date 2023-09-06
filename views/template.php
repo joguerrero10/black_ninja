c1fcb5f5568cd176c086fd97a80ab6f88140c214
joguerrero10 <joelg100314@gmail.com>
2023-09-06 16:45:43 -0700

    ordenar

---
+++ views/template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BLACK NINJA | JUEGO DE PLATAFORMA</title>
+
+  <link rel="icon" href="views/img/intro/favicon.png">
+
+  <link href="views/css/style.cs" type="text/css" rel="stylesheet" media="">
+
+  <link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
+</head>
+
+<body>
+  hola
+</body>
+
+</html>
