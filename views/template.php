286e8a7eee95c74f7f9b3a3b11ed317d1d5b6232
joguerrero10 <joelg100314@gmail.com>
2023-09-06 17:06:17 -0700

    parte 3

--- views/template.php
+++ views/template.php
@@ -3,18 +3,25 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0">
   <title>BLACK NINJA | JUEGO DE PLATAFORMA</title>
 
-  <link rel="icon" href="views/img/intro/favicon.png">
+  <link rel="icon"
+    href="views/img/intro/favicon.png">
 
-  <link href="views/css/style.cs" type="text/css" rel="stylesheet" media="">
+  <link href="views/css/styles.css"
+    type="text/css"
+    rel="stylesheet"
+    media="">
 
-  <link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Patrick+Hand"
+    rel="stylesheet">
 </head>
 
 <body>
-  hola
+  <div id="frame"></div>
+  <div id="container"></div>
 </body>
 
 </html>
