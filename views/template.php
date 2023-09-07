3887bf83cd7b0b16531ef7865cb5cc5a2bea9e62
joguerrero10 <joelg100314@gmail.com>
2023-09-07 09:58:31 -0700

    maquetar parte del login

--- views/template.php
+++ views/template.php
@@ -3,25 +3,36 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport"
-    content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>BLACK NINJA | JUEGO DE PLATAFORMA</title>
 
-  <link rel="icon"
-    href="views/img/intro/favicon.png">
+  <link rel="icon" href="views/img/intro/favicon.png">
 
-  <link href="views/css/styles.css"
-    type="text/css"
-    rel="stylesheet"
-    media="">
+  <link href="views/css/styles.css" type="text/css" rel="stylesheet" media="">
 
-  <link href="https://fonts.googleapis.com/css?family=Patrick+Hand"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
 </head>
 
 <body>
-  <div id="frame"></div>
-  <div id="container"></div>
+  <div class="frame"></div>
+
+  <div class="container">
+    <?php
+    include './modules/login.php';
+    ?>
+  </div>
+
+  <!--=====================================
+	CREDITOS
+	======================================-->
+
+  <footer>
+    <center>
+      <p>Juego desarrollado por Inovix <span>©copyright</span> </p>
+      <!-- | <a href="https://tutorialesatualcance.com" target="_blank">www.tutorialesatualcance.com</a> -->
+    </center>
+
+  </footer>
 </body>
 
 </html>
