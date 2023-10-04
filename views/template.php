6a8821976693ce87477b90efd85e8e299d59d5e0
joguerrero10 <joelg100314@gmail.com>
2023-10-03 20:38:17 -0700

    se elimino la clase text-center

--- views/template.php
+++ views/template.php
@@ -3,40 +3,22 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport"
-    content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>BLACK NINJA | JUEGO DE PLATAFORMA</title>
 
-  <link rel="icon"
-    href="views/img/intro/favicon.png">
+  <link rel="icon" href="views/img/intro/favicon.png">
 
-  <link href="views/css/styles.css"
-    type="text/css"
-    rel="stylesheet"
-    media="">
+  <link href="views/css/styles.css" type="text/css" rel="stylesheet" media="">
 
-  <link href="views/css/login.css"
-    type="text/css"
-    rel="stylesheet"
-    media="">
+  <link href="views/css/login.css" type="text/css" rel="stylesheet" media="">
 
-  <link href="views/css/start.css"
-    type="text/css"
-    rel="stylesheet"
-    media="">
+  <link href="views/css/start.css" type="text/css" rel="stylesheet" media="">
 
-  <link href="views/css/footer.css"
-    type="text/css"
-    rel="stylesheet"
-    media="">
+  <link href="views/css/footer.css" type="text/css" rel="stylesheet" media="">
 
-  <link href="https://fonts.googleapis.com/css?family=Patrick+Hand"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
-    crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 
 <body>
@@ -82,12 +64,10 @@
 	======================================-->
 
   <footer>
-    <p class="text-center">Juego desarrollado por Inovix <span>©copyright</span> </p>
+    <p>Juego desarrollado por Inovix <span>©copyright</span> </p>
   </footer>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   <script src="views/js/login.js"></script>
   <script src="views/js/variables_properties.js"></script>
 
