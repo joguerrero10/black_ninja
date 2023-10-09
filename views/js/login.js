d78d170c64a33380f1dc04af01623d963cf0c0f9
joguerrero10 <joelg100314@gmail.com>
2023-10-08 23:40:03 -0700

    graficando canvas 3

--- views/js/login.js
+++ views/js/login.js
@@ -44,6 +44,10 @@ const login = {
 		PLANO 3
 		=============================================*/
 
+    /*=============================================
+		PLANO 3
+		=============================================*/
+
     data.plane3 = new Image();
     data.plane3.src = "views/img/level" + level + "/plano3.png";
     data.plane3.onload = function () {
@@ -74,29 +78,29 @@ const login = {
 		PLANO 2
 		=============================================*/
 
-    datos.plane2 = new Image();
-    datos.plane2.src = "views/img/level" + level + "/plano2.png";
-    datos.plane2.onload = () => {
+    data.plane2 = new Image();
+    data.plane2.src = "views/img/level" + level + "/plano3.png";
+    data.plane2.onload = function () {
       ctx.drawImage(
-        datos.plane2,
+        data.plane2,
         0,
         0,
-        datos.plane2.naturalWidth,
-        datos.plane2.naturalHeight
+        data.plane2.naturalWidth,
+        data.plane2.naturalHeight
       );
       ctx.drawImage(
-        datos.plane2,
+        data.plane2,
         1000,
         0,
-        datos.plane2.naturalWidth,
-        datos.plane2.naturalHeight
+        data.plane2.naturalWidth,
+        data.plane2.naturalHeight
       );
       ctx.drawImage(
-        datos.plane2,
+        data.plane2,
         2000,
         0,
-        datos.plane2.naturalWidth,
-        datos.plane2.naturalHeight
+        data.plane2.naturalWidth,
+        data.plane2.naturalHeight
       );
     };
 
@@ -104,30 +108,210 @@ const login = {
 		PLANO 1
 		=============================================*/
 
-    datos.plane1 = new Image();
-    datos.plane1.src = "views/img/level" + level + "/plano1.png";
-    datos.plane1.onload = () => {
+    data.plane1 = new Image();
+    data.plane1.src = "views/img/level" + level + "/plano1.png";
+    data.plane1.onload = function () {
       ctx.drawImage(
-        datos.plane1,
+        data.plane1,
         0,
         0,
-        datos.plane1.naturalWidth,
-        datos.plane1.naturalHeight
+        data.plane1.naturalWidth,
+        data.plane1.naturalHeight
       );
       ctx.drawImage(
-        datos.plane1,
+        data.plane1,
         1000,
         0,
-        datos.plane1.naturalWidth,
-        datos.plane1.naturalHeight
+        data.plane1.naturalWidth,
+        data.plane1.naturalHeight
       );
       ctx.drawImage(
-        datos.plane1,
+        data.plane1,
         2000,
         0,
-        datos.plane1.naturalWidth,
-        datos.plane1.naturalHeight
+        data.plane1.naturalWidth,
+        data.plane1.naturalHeight
       );
     };
+
+    /*=============================================
+		DETALLES
+		=============================================*/
+
+    data.details = new Image();
+    data.details.src = "views/img/level" + level + "/detalles.png";
+
+    if (level === 1) {
+      let xhr_details = new XMLHttpRequest();
+      xhr_details.open("GET", "views/js/json/blocksDetailsLevel1.json", true);
+    }
+
+    if (level === 2) {
+      let xhr_details = new XMLHttpRequest();
+      xhr_details.open("GET", "views/js/json/blocksDetailsLevel2.json", true);
+    }
+
+    if (level === 3) {
+      let xhr_details = new XMLHttpRequest();
+      xhr_details.open("GET", "views/js/json/blocksDetailsLevel3.json", true);
+    }
+
+    xhr_details?.send();
+
+    xhr_details.onreadystatechange = function () {
+      if (xhr_details.readyState == 4 && xhr_details.status == 200) {
+        data.blocksDetails = JSON.parse(xhr_details.responseText);
+      }
+    };
+
+    data.details.onload = function () {
+      for (let i = 0; i < data.blocksDetails.length; i++) {
+        ctx.drawImage(
+          data.details,
+          data.blocksDetails[i].x,
+          data.blocksDetails[i].y,
+          data.blocksDetails[i].width,
+          data.blocksDetails[i].height
+        );
+      }
+    };
+
+    /*=============================================
+		BLOQUES
+		=============================================*/
+
+    data.texturePlatform = new Image();
+    data.texturePlatform.src =
+      "views/img/level" + level + "/texturaPlataforma.jpg";
+
+    if (level === 1) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "views/js/json/blocksLevel1.json", true);
+    }
+
+    if (level === 2) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "views/js/json/blocksLevel2.json", true);
+    }
+
+    if (level === 3) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "views/js/json/blocksLevel3.json", true);
+    }
+
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        // El método JSON.parse() analiza una cadena de texto como JSON, transformando opcionalmente el valor producido por el análisis.
+
+        data.blocks = JSON.parse(xhr.responseText);
+      }
+    };
+
+    data.texturePlatform.onload = function () {
+      for (let i = 0; i < data.bloques.length; i++) {
+        ctx.drawImage(
+          data.texturePlatform,
+          data.blocks[i].x,
+          data.blocks[i].y,
+          data.blocks[i].width,
+          data.blocks[i].height
+        );
+      }
+    };
+
+    /*=============================================
+		JUGADOR
+		=============================================*/
+
+    data.imgPlayer = new Image();
+    data.imgPlayer.src = "views/img/player/stop_right.png";
+    data.imgPlayer.onload = function () {
+      ctx.drawImage(
+        data.imgPlayer,
+        0,
+        0,
+        100,
+        100,
+        data.player_x,
+        data.player_y,
+        data.player_width,
+        data.player_high
+      );
+    };
+    /*=============================================
+		PLANO 0
+		=============================================*/
+
+    data.plane0 = new Image();
+    data.plane0.src = "views/img/level" + level + "/plano0.png";
+    data.plane0.onload = function () {
+      ctx.drawImage(
+        data.plane0,
+        0,
+        0,
+        data.plane0.naturalWidth,
+        data.plane0.naturalHeight
+      );
+      ctx.drawImage(
+        data.plane0,
+        1000,
+        0,
+        data.plane0.naturalWidth,
+        data.plane0.naturalHeight
+      );
+      ctx.drawImage(
+        data.plane0,
+        2000,
+        0,
+        data.plane0.naturalWidth,
+        data.plane0.naturalHeight
+      );
+    };
+
+    /*=============================================
+		PRELOAD
+		=============================================*/
+
+    let uploadFiles = [
+      data.plane0,
+      data.texturePlatform,
+      data.details,
+      data.plane1,
+      data.plane2,
+      data.plane3,
+    ];
+    let numberFiles = 0;
+    let percentage = 0;
+
+    for (let i = 0; i < uploadFiles.length; i++) {
+      uploadFiles[i].addEventListener("load", precarga);
+    }
+
+    function precarga(e) {
+      // setInterval(function(){
+
+      numberFiles++;
+      percentage = 100 / uploadFiles.length;
+
+      document.querySelector("#load span").innerHTML =
+        Math.ceil(percentage * numberFiles) + "%";
+      document.querySelector("#load meter").value = Math.ceil(
+        percentage * numberFiles
+      );
+
+      if (numberFiles == uploadFiles.length) {
+        document.querySelector("#lienzo").style.display = "block";
+
+        document.querySelector("#load").style.opacity = 0;
+
+        setTimeout(function () {
+          document.querySelector("#load").style.display = "none";
+        }, 10);
+      }
+
+      // },1000)
+    }
   },
 };
--- views/template.php
+++ views/template.php
@@ -3,22 +3,40 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0">
   <title>BLACK NINJA | JUEGO DE PLATAFORMA</title>
 
-  <link rel="icon" href="views/img/intro/favicon.png">
+  <link rel="icon"
+    href="views/img/intro/favicon.png">
 
-  <link href="views/css/styles.css" type="text/css" rel="stylesheet" media="">
+  <link href="views/css/styles.css"
+    type="text/css"
+    rel="stylesheet"
+    media="">
 
-  <link href="views/css/login.css" type="text/css" rel="stylesheet" media="">
+  <link href="views/css/login.css"
+    type="text/css"
+    rel="stylesheet"
+    media="">
 
-  <link href="views/css/start.css" type="text/css" rel="stylesheet" media="">
+  <link href="views/css/start.css"
+    type="text/css"
+    rel="stylesheet"
+    media="">
 
-  <link href="views/css/footer.css" type="text/css" rel="stylesheet" media="">
+  <link href="views/css/footer.css"
+    type="text/css"
+    rel="stylesheet"
+    media="">
 
-  <link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Patrick+Hand"
+    rel="stylesheet">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+    crossorigin="anonymous">
 </head>
 
 <body>
@@ -64,10 +82,12 @@
 	======================================-->
 
   <footer>
-    <p>Juego desarrollado por Inovix <span>©copyright</span> </p>
+    <p class="text-center">Juego desarrollado por Inovix <span>©copyright</span> </p>
   </footer>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+    crossorigin="anonymous"></script>
   <script src="views/js/login.js"></script>
   <script src="views/js/variables_properties.js"></script>
 
