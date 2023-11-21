8e949d1c3b5f4279fa2fae139317f9cb84a95deb
joguerrero10 <joelg1014@hotmail.com>
2023-11-21 23:06:40 +0500

    login con facebook

--- views/template.php
+++ views/template.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<!--=====================================
@@ -7,15 +8,15 @@
 	======================================-->
 
 	<meta property="og:url" content="http://ninjablack.byethost7.com" />
-	<meta property="og:type"  content="article" />
-	<meta property="og:title"   content="black ninja" />
-	<meta property="fb:app_id"  content="1922319868005008" />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="black ninja" />
+	<meta property="fb:app_id" content="750291470258589" />
 
 	<!--=====================================
 	PANTALLA DE INICIO DISPOSITIVOS TOUCH
 	======================================-->
 
-	<meta name="viewport" content="user-scalable=no, maximum-scale=1.0, minimal-ui"/>
+	<meta name="viewport" content="user-scalable=no, maximum-scale=1.0, minimal-ui" />
 
 	<!-- for ios 7 style, multi-resolution icon of 152x152 -->
 	<meta name="apple-mobile-web-app-capable" content="yes">
@@ -36,6 +37,7 @@
 	<script src="views/js/screenfull.min.js"></script>
 
 </head>
+
 <body>
 
 	<!--=====================================
@@ -55,71 +57,72 @@
 	======================================-->
 
 	<div id="contenedor">
-		
+
 		<?php
 
-			if(isset($_GET["validar"])){
+		if (isset($_GET["validar"])) {
 
-				switch($_GET["validar"]){
-					case "inicio":
+			switch ($_GET["validar"]) {
+				case "inicio":
 					include "modules/inicio.php";
 					break;
-					case "salir":
+				case "salir":
 					include "modules/salir.php";
 					break;
-					default:
+				default:
 					include "modules/ingreso.php";
-				}
-
-			}else{
-
-				include "modules/ingreso.php";
-			
 			}
-	
+		} else {
+
+			include "modules/ingreso.php";
+		}
+
 		?>
 
 	</div>
 
-	
+
 
 	<!--=====================================
 	CREDITOS
 	======================================-->
 
 	<footer>
-		
+
 		<center>
-		<p>Juego desarrollado por Inovix</p>
+			<p>Juego desarrollado por Inovix</p>
 		</center>
 
 	</footer>
 
-<script src="views/js/variables_y_propiedades.min.js"></script>
-<script src="views/js/inicio.min.js"></script>
-<script src="views/js/juego.min.js"></script>
-<script src="views/js/lienzo.min.js"></script>
-<script src="views/js/ampliarCanvas.min.js"></script>
+	<script src="views/js/variables_y_propiedades.min.js"></script>
+	<script src="views/js/inicio.min.js"></script>
+	<script src="views/js/juego.min.js"></script>
+	<script src="views/js/lienzo.min.js"></script>
+	<script src="views/js/ampliarCanvas.min.js"></script>
 
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1922319868005008',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();   
-  };
+	<script>
+		window.fbAsyncInit = function() {
+			FB.init({
+				appId: '750291470258589',
+				cookie: true,
+				xfbml: true,
+				version: 'v2.8'
+			});
+			FB.AppEvents.logPageView();
+		};
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-</script>
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {
+				return;
+			}
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "//connect.facebook.net/en_US/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
 </body>
+
 </html>
